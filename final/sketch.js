29b7e358911eccf196ae2b6faf3535dd878aefee
Sarah McNutt <mcnus603@newschool.edu>
2018-05-13 02:46:34 -0400

    final

--- final/sketch.js
+++ final/sketch.js
@@ -2,30 +2,38 @@
 //Code 2 Final
 var allTheData;
 var images = [];
+var DOMMainImages = [];
 var titles = [];
 var geometries = [];
 var materials = [];
 var textGeo;
 var textArray = [];
-var hello = ["H", "E", "L", "L", "O"];
-var randomNums = [0.004, 0.001, 0.006, 0.002, 0.005];
+var sections = ["home", "aWork", "about"];
+var currentSection = "aWork";
 
 
-//GET FROM DOM
-var pg1 = document.getElementById('pg1');
-var pg1W = pg1.offsetWidth;
-var pg1H = pg1.offsetHeight;
 
-var pg2 = document.getElementById('pg2');
-var pg2W = pg2.offsetWidth;
-var pg2H = pg2.offsetHeight;
-
-//Position Calculations
-function posCalc(){
-  var width = document.innerWidth;
-  var colLength = width/3;
-  pg2.appendChild()
+window.onload = function() {
+ sectionCheck();  
 }
+
+window.addEventListener('click', sectionCheck());
+
+
+function sectionCheck () {
+      //WHY IS MY SWITCH CASE NOT WORKING?
+  drawContent(currentSection);
+  //   if (currentSection == "home") {
+  //   displayHomepage();
+  //   loadMainImages();
+
+  // }
+
+  // if (currentSection == "aWork") {
+  //   displayAPieceOfWork();
+  // }
+}
+
 
 
 //XMLHTTPRequest
@@ -39,66 +47,104 @@ request.onload = function(event) {
   allTheData = JSON.parse(event.target.responseText);
   for (var i =0; i< allTheData.content.work.length; i++) {
     var theImage = allTheData.content.work[i].mainImage;
-    var theTitle = allTheData.content.wotk[i].title;
+    // var theTitle = allTheData.content.wotk[i].title;
     images.push(theImage);
-    titles.push(theTitle);
 
+    // titles.push(theTitle);
   }
+  loadMainImages();
 }
 request.send();
 
 
-//THREE JS 
-
-//HELLO ANIMATION INIT
-var HelloScene = new THREE.Scene();
-var HelloCamera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
-var HelloRenderer = new THREE.WebGLRenderer(); 
-HelloRenderer.setSize(pg1W, pg1H);
-pg1.appendChild(HelloRenderer.domElement);
-
-//WORK ANIMATION INIT
-var WorkScene = new THREE.Scene();
-WorkScene.background = new THREE.Color(0xffffff);
-var WorkCamera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
-var WorkRenderer = new THREE.WebGLRenderer();
-WorkRenderer.setSize(pg2W, pg2H);
-pg2.appendChild(WorkRenderer.domElement);
-
-//create the shapes
-// function createS
-//   for(var i = 0; i < images.length; i++) {
-//     var geometry = new THREE.PlaneGeometry(1, 1, 1);
-//     var material = new THREE.MeshBasicMaterial({color:0xffffff}, wireFrame = true);
-//     // var material = new THREE.TextureLoader().load(images[i]);
-//     // var cube = new THREE.Mesh(geometry, material);
-//     // WorkScene.add(cube);
-//     var x = i;
-//     geometries.push(x);
-//     materials.push(material);
-//   }
-function loadFont() {
- var loader = new THREE.FontLoader();
- loader.load('franklinGothic.json', function (res) {
-   font = res;
-   for (var i = 0; i < hello.length; i++) {
-    var theText = hello[i];
-    createText(theText, i); 
-   }
-   
- });
+//STATE MACHINE
+function drawContent() {
+  switch(currentSection) {
+    case 'home': 
+      displayHomepage();
+      break;
+    case 'aWork':
+      break;
+    case 'about':
+      break;
+  }
 }
 
-var textMaterial = new THREE.MeshBasicMaterial({color: 0xfef2f6});
-var ambientLight = new THREE.AmbientLight(0xfef2f6, 1.0);
-HelloScene.add(ambientLight);
+function hideEl(el) {
+  el.style.display = 'none';
+}
 
+function displayEl(el){
+  el.style.display = 'block';
+}
+
+function aWorkClicked() {
+}
+
+function displayHomepage() {
+  //HELLO 
+  // displayHello(); 
+  //WORK
+  loadMainImages();
+}
+
+function displayHello() {
+  //GET PG 1
+  var pg1 = document.getElementById('pg1');
+  displayEl(pg1);
+
+  pg1.addEventListener("scroll", function(e) {
+    console.log("a scroll");
+  } );
+
+  var pg1W = pg1.offsetWidth;
+  var pg1H = pg1.offsetHeight;
+  var hello = ["H", "E", "L", "L", "O"];
+  var randomNums = [0.004, 0.001, 0.006, 0.002, 0.005];
+
+  //Need some type of position calculation here for the hello
+  //TKTK
+
+  //HELLO ANIMATION INIT
+  var HelloScene = new THREE.Scene();
+  // HelloScene.background = new THREE.Color(0x000000);
+  var HelloCamera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  var HelloRenderer = new THREE.WebGLRenderer(); 
+  HelloRenderer.setSize(pg1W, pg1H);
+  pg1.appendChild(HelloRenderer.domElement);
+
+  //LOAD FONT
+  function loadFont() {
+    var loader = new THREE.FontLoader();
+    loader.load('helvetica.json', function (res) {
+      font = res;
+      for (var i = 0; i < hello.length; i++) {
+        var theText = hello[i];
+        createText(theText, i); 
+      }
+    });
+  }
+  //TEXT MATERIAL
+  var textMaterial = new THREE.MeshPhongMaterial({color: 0xcc669c});
+  materials = [
+    new  THREE.MeshPhongMaterial ({color:0xf49f9f, flatShading:true}), //front
+    new THREE.MeshPhongMaterial({color:0xefbdbd}) //side
+  ]
+  var ambientLight = new THREE.AmbientLight(0x000000, 5.0);
+  var directionalLight = new THREE.DirectionalLight( 0xcc6464, 5 );
+  var light = new THREE.DirectionalLight(0xc266cc, 3);
+  
+  // HelloScene.add(ambientLight);
+  // HelloScene.add( directionalLight);
+  // HelloScene.add( light);
+  
+  //CREATE TEXTGEOMETRY
   function createText(theText, i) {
     textGeo = new THREE.TextGeometry( theText, {
       font: font,
-      size: 60,
-      height: 20,
-      curveSegments:4,
+      size: 80,
+      height: 10,
+      curveSegments:20,
       weight: "normal",
       bevelThickness:.2,
       bevelSize:1.5,
@@ -106,67 +152,122 @@ HelloScene.add(ambientLight);
       bevelEnabled:true
     });
     textGeo.computeBoundingBox();
-    textGeo.computeVertexNormals();
-    var text = new THREE.Mesh(textGeo, material)
+    // textGeo.computeVertexNormals();
+    var text = new THREE.Mesh(textGeo, textMaterial)
 
     text.position.x = (i *100);
     text.position.x -= window.innerWidth/7;
 
-    // text.position.x = -textGeo.boundingBox.max.x/2;
     text.position.x += Math.random(0, 100);
-    text.castShadow = true;
     HelloScene.add(text);
     textArray.push(text);
   }
+  //MOVE CAMERA
+  HelloCamera.position.z = 200;
 
+  //GAME LOGIC
+  var update = function (){
+    for (var i =0; i < textArray.length; i++) {
+      var r =  randomNums[i];
+      var theText = textArray[i];
+      theText.rotation.y += r;
+      theText.rotation.z += r;
+    }
+  };
 
-
-var geometry = new THREE.PlaneGeometry(214.8, 150.0, 1);
-var material = new THREE.MeshBasicMaterial({map: new THREE.TextureLoader().load('images/frankenstien.jpg'), side:THREE.DoubleSide})
-var cube = new THREE.Mesh(geometry, material);
-cube.position.x = 20;
-
-WorkScene.add(cube);
-
-
-WorkCamera.position.z = 300;
-HelloCamera.position.z = 200;
-
-//GAME LOGIC
-var update = function (){
-  for (var i =0; i < textArray.length; i++) {
-    var r =  randomNums[i];
-    var theText = textArray[i];
-    theText.rotation.y += r;
-     theText.rotation.z += r;
-
-
+  var render = function() {
+    HelloRenderer.render(HelloScene, HelloCamera);
   }
 
-  cube.rotation.y += 0.004;
-};
-
-//DRAW SCENE
-var render = function() {
-  HelloRenderer.render(HelloScene, HelloCamera);
-  WorkRenderer.render(WorkScene, WorkCamera);
-}
-
-//RUN GAME LOOP (update, render, repeat)
-var GameLoop = function() {
-  requestAnimationFrame(GameLoop);
+  //GAME LOOP
+  var GameLoop = function() {
+    requestAnimationFrame(GameLoop);
     update();
     render();
-};
-loadFont();
-GameLoop();
+  };
 
-//CANVAS RESIZE
+  //CANVAS RESIZE
+  window.addEventListener('resize', function () {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    HelloRenderer.setSize(width, height);
+    HelloCamera.aspect= width/height;
+    HelloCamera.updateProjectionMatrix();
+  });
+
+  loadFont();
+  GameLoop();
+}
+
+function displayWork() {
+  //GET PG 2
 
 
+  var pg2 = document.getElementById('pg2');
+  displayEl(pg2);
+
+  var pg2W = pg2.offsetWidth;
+  var pg2H = pg2.offsetHeight;
+
+  //Calculate the Position for the work
+  function posCalc(){
+    var width = document.innerWidth;
+    var colLength = width/3;
+  }
+
+  //WORK ANIMATION INIT -- do I want to use THREE.JS here? 
+  // var WorkScene = new THREE.Scene();
+  // WorkScene.background = new THREE.Color(0xffffff);
+  // var WorkCamera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000)
+  // var WorkRenderer = new THREE.WebGLRenderer();
+  // WorkRenderer.setSize(pg2W, pg2H);
+  // pg2.appendChild(WorkRenderer.domElement);
 
 
+  // WorkCamera.position.z = 300;
+}
 
 
+function displayAPieceOfWork() {
+  document.body.style="background-color: blue";
+}
 
+function displayAboutPage() {
+}
+
+
+//CREATE MAIN IMAGES
+//Why is this function not running
+//then add event listners
+function loadMainImages() {
+var pg2 = document.getElementById('pg2');
+  
+  for (var i = 0; i < images.length; i++) {
+    var anImage = document.createElement('img');
+    anImage.setAttribute('src', images[i]);
+    anImage.setAttribute('left', window.innerWidth);
+    anImage.setAttribute('top', i*200);
+    anImage.classList.add('mainImage');
+
+    pg2.appendChild(anImage);
+    DOMMainImages.push(anImage);
+    anImage.addEventListener('click', function() {
+      currentSection = "aWork";
+      console.log("clicked");
+    });
+   
+  }
+}
+
+// function displayMainImages() {
+//   for(var i =0; i < DOMMainImages.length; i++) {
+//     displayEl(DOMMainImages[i]);
+//   }
+// }
+
+// function hideMainImages() {
+//   for(var i =0; i < DOMMainImages.length; i++) {
+//     hideEl(DOMMainImages[i]);
+//   }
+// }
 
---
+++ final/sketch2.js
@@ -0,0 +1,419 @@
+//Get State Machine working 
+//parse all dom elements 
+//DOM elements
+var pg1; 
+var pg2;
+var header;
+var footer;
+var title;
+var text;
+var slideshow;
+var entryContainer;
+var yPos = 0;
+var index = 0;
+var home;
+var about;
+var work;
+var aboutPg;
+var cameraZ = 300;
+
+var DOMMainImages = [];
+var DOMOtherImages = [];
+var homeElements = [];
+var workElements = [];
+var aboutElements = [];
+
+//data varriables
+var allTheData;
+var images = [];
+var titles = [];
+var textEntries = [];
+var otherImages = [];
+var titleDivs;
+
+var currentSection = "home";
+
+//THREE.js varriables
+var geometries = [];
+var materials = [];
+var textGeo;
+var textArray = [];
+
+
+//XML HTTP request
+var request;
+request = new XMLHttpRequest();
+request.open('GET', 'portfolio.json', true);
+//load into arrays
+request.onload = function(event) {
+	allTheData = JSON.parse(event.target.responseText);
+	for (var i =0; i< allTheData.content.work.length; i++) {
+		var theImage = allTheData.content.work[i].mainImage;
+		images.push(theImage);
+		var theTitle = allTheData.content.work[i].title;
+		titles.push(theTitle);
+		var theText = allTheData.content.work[i].text;
+		textEntries.push(theText);
+		var sImages = [];
+		sImages = allTheData.content.work[i].otherImages;
+		otherImages.push(sImages);
+	}
+
+	parseDOMElements();
+	eventListnersToDOM();
+	drawContent();
+	displayHello();
+
+}
+request.send();
+
+
+//Get DOM Elements
+function parseDOMElements () {
+	pg1 = document.getElementById('pg1');
+	homeElements.push(pg1);
+
+	pg2 = document.getElementById('pg2');
+	homeElements.push(pg2);
+	 // header = document.getElementById('header');
+
+	footer = document.getElementById('footer');
+	 
+	entryContainer = document.getElementById('entryContainer');
+	workElements.push(entryContainer);
+
+	title = document.getElementById('title');
+	workElements.push(title);
+
+	text = document.getElementById('text');
+	workElements.push(text);
+
+	slideshow = document.getElementById('slideshow');
+	workElements.push(slideshow); 
+
+	home = document.getElementById('home');
+	about = document.getElementById('about');
+	work = document.getElementById('work');
+
+	aboutPg = document.getElementById('aboutPg');
+
+
+	loadMainImages();
+	loadOtherImages();
+	
+
+}
+
+function loadMainImages() {  
+  for (var i = 0; i < images.length; i++) {
+    var anImage = document.createElement('img');
+    anImage.setAttribute('src', images[i]);
+    anImage.setAttribute('left', window.innerWidth);
+    anImage.setAttribute('top', i*200);
+    anImage.classList.add('mainImage');
+    anImage.id = [i];
+    DOMMainImages.push(anImage);
+    homeElements.push(anImage);
+
+    var theImageW = anImage.width;
+    var theImageH = anImage.height;
+
+    // var theTitle = document.createElement('div');
+    // theTitle.innerHTML = titles[i];
+    // theTitle.setAttribute('width', theImageW);
+    // titleDivs.push(theTitle, theImageH);
+  }
+
+ 	positionImages();
+
+ 	for (var i = 0; i < DOMMainImages.length; i++) {
+ 		var anImage = DOMMainImages[i];
+ 		pg2.appendChild(anImage);
+ 	}
+}
+
+function loadOtherImages() {
+	//create element
+	for (var i = 0; i < otherImages.length; i++) {
+		var anArray = [];
+		for(var j = 0; j < otherImages[i].length; j++) {
+			var anImage = document.createElement('img');
+    	anImage.setAttribute('src', otherImages[i][j]);
+
+    	anArray.push(anImage);
+    	slideshow.appendChild(anImage);
+    	anImage.classList.add('otherImage');
+
+
+		}
+		DOMOtherImages.push(anArray);
+	}
+	//append
+	// for(var i = 0; i < DOMOtherImages.length; i++) {
+	// 	for(var j = 0; j < DOMOtherImages[i].length; j++) {
+	// 		var anEl = DOMMainImages[i][j]
+	// 	}
+	// }
+
+}
+
+function hideOtherImages() {
+	for(var i = 0; i <otherImages.length; i++) {
+		for (var j = 0; j < otherImages[i].length; j++) {
+			var anImage = otherImages[i][j];
+			hideEl(anImage);
+		}
+	}
+}
+
+//State Machine
+function drawContent() {
+	switch(currentSection) {
+		case 'home': 
+		displayHomepage();
+		positionImages();
+		aboutPg.style.display = "none";
+		entryContainer.style.display = "none";
+			break;
+		case 'aWork':
+    	displayAWork();
+    	pg1.style.display = "none";
+			pg2.style.display = "none";
+			aboutPg.style.display = "none";
+    	break;
+    case 'about':
+    	displayAbout();
+    	pg1.style.display = "none";
+			pg2.style.display = "none";
+			entryContainer.style.display = "none";
+    	break;
+    }
+}
+
+function eventListnersToDOM() {
+	//Images 
+	for (var i = 0; i < DOMMainImages.length; i ++) {
+		var anImage = DOMMainImages[i];
+
+		anImage.addEventListener('click', function(){
+			currentSection = "aWork";
+			index = this.id;
+		});
+	}
+	//Nav bar 
+	home.addEventListener('click', function(){
+		currentSection = "home";
+	});
+	work.addEventListener('click', function(){
+		currentSection = "home";
+	});
+	about.addEventListener('click', function(){
+		currentSection = "about"
+	});
+}
+
+document.addEventListener('click', function() {
+	drawContent();
+});
+
+function hideEl(el) {
+	el.style.display = "none";
+}
+
+function displayInlineBlock(el){
+	el.style.display = "inline-block";
+}
+
+function displayBlock(el){
+	el.style.display = "block";
+}
+
+
+function displayHomepage() {
+	for (var i = 0; i < homeElements.length; i++) {
+		var theEl = homeElements[i];
+		displayInlineBlock(theEl)
+	}
+	for (i = 0; i < workElements; i++) {
+		var theEl = workElements[i];
+		hideEl(theEl);
+	}
+
+}
+
+function displayAWork() {
+
+	for (i = 0; i < homeElements; i++) {
+		var theEl = homeElements[i];
+		hideEl(theEl);
+	}
+	for (var i = 0; i < workElements.length; i++) {
+		var theEl = workElements[i];
+		displayBlock(theEl)
+	}
+
+	//change HTML 
+	title.innerHTML = titles[index];
+	text.innerHTML = textEntries[index]
+	for (var i = 0; i < DOMOtherImages[index].length; i++) {
+		var theEl = DOMOtherImages[index][i];
+		displayBlock(theEl);
+	}
+}
+
+
+
+function displayHello() {
+  //GET PG 1
+  var pg1W = pg1.offsetWidth;
+  var pg1H = pg1.offsetHeight;
+  var hello = ["H", "E", "L", "L", "O"];
+  var randomNums = [0.004, 0.001, 0.006, 0.002, 0.005];
+
+  //HELLO ANIMATION INIT
+  var HelloScene = new THREE.Scene();
+  // HelloScene.background = new THREE.Color(0xffffff);
+  var HelloCamera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+  var HelloRenderer = new THREE.WebGLRenderer(); 
+  HelloRenderer.setSize(pg1W, pg1H);
+  pg1.appendChild(HelloRenderer.domElement);
+
+  //LOAD FONT
+  function loadFont() {
+    var loader = new THREE.FontLoader();
+    loader.load('helvetica.json', function (res) {
+      font = res;
+      for (var i = 0; i < hello.length; i++) {
+        var theText = hello[i];
+        createText(theText, i); 
+      }
+    });
+  }
+  //TEXT MATERIAL
+  var textMaterial = new THREE.MeshLambertMaterial({color: 0x3694f2});
+ 
+  var ambientLight = new THREE.AmbientLight(0x000000, 50);
+ 
+  var directionalLight1 = new THREE.DirectionalLight( 0xe7b6f2, 2 );
+  directionalLight1.position.set(500, -200, 500);
+
+  var directionalLight2 = new THREE.DirectionalLight(0xc266cc, 2);
+  directionalLight2.position.set(-500, 500, 500);
+
+  var directionalLight3 = new THREE.DirectionalLight(0x505ae2, 2);
+  directionalLight3.position.set(0, 500, 500);
+
+
+  HelloScene.add(ambientLight);
+  HelloScene.add(directionalLight1);
+  HelloScene.add(directionalLight2);
+  HelloScene.add(directionalLight3);
+
+  //CREATE TEXTGEOMETRY
+  function createText(theText, i) {
+    textGeo = new THREE.TextGeometry( theText, {
+      font: font,
+      size: 60,
+      height: 20,
+      curveSegments:20,
+      weight: "normal",
+      bevelThickness:.2,
+      bevelSize:3,
+      bevelSegments:20,
+      bevelEnabled:true
+
+    });
+    textGeo.computeBoundingBox();
+    // textGeo.computeVertexNormals();
+    var text = new THREE.Mesh(textGeo, textMaterial)
+
+    text.position.x = (i *100);
+    text.position.x -= window.innerWidth/7;
+
+    text.position.x += Math.random(0, 100);
+    text.castShadow = true;
+    HelloScene.add(text);
+    textArray.push(text);
+  }
+  //MOVE CAMERA
+  HelloCamera.position.z = cameraZ;
+
+  //GAME LOGIC
+  var update = function (){
+    for (var i =0; i < textArray.length; i++) {
+      var r =  randomNums[i];
+      var theText = textArray[i];
+      theText.rotation.y += r;
+      theText.rotation.z += r;
+       HelloCamera.position.z = cameraZ;
+    }
+  };
+
+  var render = function() {
+    HelloRenderer.render(HelloScene, HelloCamera);
+  // WorkRenderer.render(WorkScene, WorkCamera);
+  }
+
+  //GAME LOOP
+  var GameLoop = function() {
+    requestAnimationFrame(GameLoop);
+    update();
+    render();
+  };
+
+  //CANVAS RESIZE
+  window.addEventListener('resize', function () {
+    var width = window.innerWidth;
+    var height = window.innerHeight;
+    HelloRenderer.setSize(width, height);
+    HelloCamera.aspect= width/height;
+    HelloCamera.updateProjectionMatrix();
+  });
+
+  loadFont();
+  GameLoop();
+}
+
+
+function positionImages() {
+	var xPos; 
+
+	for(var i = 0; i < DOMMainImages.length; i++) {
+		var theEl = DOMMainImages[i];
+		var modCalc = i%3; 
+		var offset = theEl.width/2;
+		yPos = (i *200) + "px";
+
+		if (modCalc == 0) {
+			xPos = (innerWidth/4) - offset + "px";
+		} else if (modCalc == 1) {
+			xPos = ((innerWidth/ 4) * 3) - offset + "px";
+		} else {
+			xPos = (innerWidth/2) - offset + "px";
+		}
+		theEl.style.display = "block";
+		theEl.style.left = xPos;
+		theEl.style.top =  yPos;
+	}
+
+	var longestNum = DOMMainImages.length;
+	longestHeight = longestNum * 200;
+	pg2.style.height = longestHeight + "px";
+}
+
+function displayAbout() {
+	displayInlineBlock(aboutPg);
+}
+
+
+window.onscroll = function() {
+	console.log("scroll");
+	var st = document.documentElement.scrollTop;
+	cameraZ = 300-st;
+
+	console.log(document.documentElement.scrollTop);
+}
+	// 300
+
+
+
